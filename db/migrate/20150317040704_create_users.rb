930ea57f00fe0ec76865cadd32b941010d403d09
Christina Araya <christina-araya@hotmail.com>
2015-03-17 04:48:22 +0000

    modified migration, added more views

--- db/migrate/20150317040704_create_users.rb
+++ db/migrate/20150317040704_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :password
+      t.string :email, null: false
+      t.string :password_digest, null: false
       t.boolean :admin
       t.datetime :last_signed_in_at
 
--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 20150317040704) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email"
-    t.string   "password"
+    t.string   "email",             null: false
+    t.string   "password_digest",   null: false
     t.boolean  "admin"
     t.datetime "last_signed_in_at"
     t.datetime "created_at",        null: false
