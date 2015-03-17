a1acc82bb5ca8d59ffd668f8452ea7fc8bed0ea4
Christina Araya <christina-araya@hotmail.com>
2015-03-17 04:32:33 +0000

    modified routes, models, and controllers added index view

--- app/controllers/session_pages_controller.rb
+++ app/controllers/session_pages_controller.rb
@@ -1,2 +1,25 @@
 class SessionPagesController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: session_page_params[:email])
+    if user && user.authenticate(session_page_params[:password])
+      session_page[:user_id] = user.id
+      redirect_to root_path, notice: 'Nice!'
+    else
+      redirect_to new_session_page_path, notice: 'No way!'
+    end
+  end
+
+  def destroy
+    session_page.delete(:user_id)
+    redirect_to root_path, notice: 'Bye!'
+  end
+
+  private
+
+  def session_page_params
+    params.require(:session_page).permit(:email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+resources :session_pages, only: [:new, :create, :index]
+resources :users, except: [:show]
+
+delete 'sign_out' => 'session_pages#destroy'
+
+root 'session_pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
